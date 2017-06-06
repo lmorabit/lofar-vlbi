ff8de727c2cc8ebf446570dfadb550cf5bcb8074
leah <lmorabit@gmail.com>
2017-06-06 13:58:57 +0100

    fixed MFS mode of wsclean

--- bin/self_calibration_and_imaging.py
+++ bin/self_calibration_and_imaging.py
@@ -98,7 +98,7 @@ def main():
 
     ## check if MFS needs to be turned on
     if bandwidth > freq_range:
-        ss = "wsclean -datacolumn %s -scale 0.05asec -size %i %i -trim %i %i -joinchannels -channelsout %i -fit-spectral-pol -rms-background -maxuv-l 800000 -gain 0.1 -mgain 0.85 -niter 100000 -weighting-rank-filter 3 -weighting-rank-filter-size 256 -auto-threshold 5 -multiscale -auto-mask 7 -save-component-list -reorder -name %s-CLN %s"%(data_col,imsize,imsize,imtrim,imtrim,nchan,tgtname,msname)
+        ss = "wsclean -datacolumn %s -scale 0.05asec -size %i %i -trim %i %i -joinchannels -channelsout %i -fit-spectral-pol 2 -rms-background -maxuv-l 800000 -gain 0.1 -mgain 0.85 -niter 100000 -weighting-rank-filter 3 -weighting-rank-filter-size 256 -auto-threshold 5 -multiscale -auto-mask 7 -save-component-list -reorder -name %s-CLN %s"%(data_col,imsize,imsize,imtrim,imtrim,nchan,tgtname,msname)
     else:
         ss = "wsclean -datacolumn %s -scale 0.05asec -size %i %i -trim %i %i -rms-background -maxuv-l 800000 -gain 0.1 -mgain 0.85 -niter 100000 -weighting-rank-filter 3 -weighting-rank-filter-size 256 -auto-threshold 5 -multiscale -auto-mask 7 -save-component-list -reorder -name %s-CLN %s"%(data_col,imsize,imsize,imtrim,imtrim,tgtname,msname)
 
@@ -127,7 +127,7 @@ def main():
             os.system( ss )
             imagename = "%s-CLN-PO%s"%(tgtname,str(ii+1))
             if bandwidth > freq_range:
-                ss = "wsclean -datacolumn CORRECTED_DATA -scale 0.05asec -size %i %i -trim %i %i -joinchannels -channelsout %i -fit-spectral-pol -rms-background -maxuv-l 800000 -gain 0.1 -mgain 0.85 -niter 100000 -weighting-rank-filter 3 -weighting-rank-filter-size 256 -auto-threshold 5 -multiscale -auto-mask 7 -save-component-list -reorder -name %s %s"%(imsize,imsize,imtrim,imtrim,nchan,imagename,msname)
+                ss = "wsclean -datacolumn CORRECTED_DATA -scale 0.05asec -size %i %i -trim %i %i -joinchannels -channelsout %i -fit-spectral-pol 2 -rms-background -maxuv-l 800000 -gain 0.1 -mgain 0.85 -niter 100000 -weighting-rank-filter 3 -weighting-rank-filter-size 256 -auto-threshold 5 -multiscale -auto-mask 7 -save-component-list -reorder -name %s %s"%(imsize,imsize,imtrim,imtrim,nchan,imagename,msname)
             else:
                 ss = "wsclean -datacolumn CORRECTED_DATA -scale 0.05asec -size %i %i -trim %i %i -rms-background -maxuv-l 800000 -gain 0.1 -mgain 0.85 -niter 100000 -weighting-rank-filter 3 -weighting-rank-filter-size 256 -auto-threshold 5 -multiscale -auto-mask 7 -save-component-list -reorder -name %s %s"%(imsize,imsize,imtrim,imtrim,imagename,msname)
             os.system( ss )
@@ -140,7 +140,7 @@ def main():
         os.system( ss )
         imagename = "%s-CLN-PA-final"%(tgtname)
         if bandwidth > freq_range:
-            ss = "wsclean -datacolumn CORRECTED_DATA -scale 0.05asec -size %i %i -trim %i %i -joinchannels -channelsout %i -fit-spectral-pol -rms-background -maxuv-l 800000 -gain 0.1 -mgain 0.85 -niter 100000 -weighting-rank-filter 3 -weighting-rank-filter-size 256 -auto-threshold 5 -multiscale -auto-mask 7 -save-component-list -reorder -name %s %s"%(imsize,imsize,imtrim,imtrim,nchan,imagename,msname)
+            ss = "wsclean -datacolumn CORRECTED_DATA -scale 0.05asec -size %i %i -trim %i %i -joinchannels -channelsout %i -fit-spectral-pol 2 -rms-background -maxuv-l 800000 -gain 0.1 -mgain 0.85 -niter 100000 -weighting-rank-filter 3 -weighting-rank-filter-size 256 -auto-threshold 5 -multiscale -auto-mask 7 -save-component-list -reorder -name %s %s"%(imsize,imsize,imtrim,imtrim,nchan,imagename,msname)
         else:
             ss = "wsclean -datacolumn CORRECTED_DATA -scale 0.05asec -size %i %i -trim %i %i -rms-background -maxuv-l 800000 -gain 0.1 -mgain 0.85 -niter 100000 -weighting-rank-filter 3 -weighting-rank-filter-size 256 -auto-threshold 5 -multiscale -auto-mask 7 -save-component-list -reorder -name %s %s"%(imsize,imsize,imtrim,imtrim,imagename,msname)
         os.system( ss )
