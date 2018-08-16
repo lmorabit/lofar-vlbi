c425fdbe88ad5439cd03258d59e5a4da25377f25
lmorabit <lmorabit@gmail.com>
2018-08-16 10:43:48 +0000

    changed download cats to a plugin and updated to use astropy tables instead of pandas

--- plugins/PipelineStep_DownloadCats.py
+++ plugins/PipelineStep_DownloadCats.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python
-from scipy import stats  
+#from scipy import stats  
 import os, sys, logging
 import numpy as np
-import csv
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+#import csv
+#import matplotlib
+#matplotlib.use('Agg')
+#import matplotlib.pyplot as plt
 #from lmfit import SkewedGaussianModel
 import pyvo as vo
 import pyrap.tables as pt
-import pandas
+from astropy.table import Table, Column, vstack, unique
 import argparse
+from lofarpipe.support.data_map import DataMap
+from lofarpipe.support.data_map import DataProduct
 
 def grab_coo_MS(MS):
     """
@@ -166,13 +168,24 @@ def find_close_objs(lo, lb, tolerance=5.):
     lotssflux=np.array(lo['Total_flux'])
     lotsspeakflux=np.array(lo['Peak_flux'])
     ## source ID, rms, and resolved information
-    Source_ID=np.array(lo['Source_id'])
+    src_id=np.array(lo['Source_id'])
     rms=np.array(lo['Isl_rms'])
     resolved=np.array(lo['Resolved'])
     lbcsid=np.array(lb['ObsID'])
 
-    colnames = ['Source_ID','LBCS_RA','LBCS_DEC','LOTSS_RA','LOTSS_DEC','Delta_Position','Total_flux','Peak_flux','peak_to_total','Isl_rms','Resolved']
-    result = pandas.DataFrame(columns=colnames)
+    ## astropy.tables.Table is fast at adding columns, less fast at adding rows -- since it has to make a copy each time
+    Source_id = []
+    LBCS_ID = []
+    LBCS_RA = []
+    LBCS_DEC = []
+    LOTSS_RA = []
+    LOTSS_DEC = []
+    Delta_Position = []
+    Total_flux = []
+    Peak_flux = []
+    peak_to_total = []
+    Isl_rms = []
+    Resolved = []
 
     if len(lbcsRA.shape) > 0:
         ## there's more than one LBCS sources, iterate through the list
@@ -187,12 +200,35 @@ def find_close_objs(lo, lb, tolerance=5.):
 
             ## check if it's less than tolerance (in arcsec)
             if delta_position[c_idx] < tolerance:
-		
-                tmp_row = [Source_ID[c_idx][0],lbcsRA[x],lbcsDEC[x],lotssRA[c_idx][0],lotssDEC[c_idx][0],np.min(delta_position),lotssflux[c_idx][0],lotsspeakflux[c_idx][0],(lotsspeakflux[c_idx][0]/lotssflux[c_idx][0]), rms[c_idx][0], resolved[c_idx][0] ]
-                result.loc[x] = tmp_row
+	        Source_id.append(src_id[c_idx][0])
+		LBCS_ID.append(lbcsid[x])
+		LBCS_RA.append(lbcsRA[x])
+		LBCS_DEC.append(lbcsDEC[x])
+	        LOTSS_RA.append(lotssRA[c_idx][0])
+	        LOTSS_DEC.append(lotssDEC[c_idx][0])
+	        Delta_Position.append(np.min(delta_position))
+	        Total_flux.append(lotssflux[c_idx][0])
+	        Peak_flux.append(lotsspeakflux[c_idx][0])
+	        peak_to_total.append((lotsspeakflux[c_idx][0]/lotssflux[c_idx][0]))
+		Isl_rms.append(rms[c_idx][0])
+	        Resolved.append(resolved[c_idx][0])
             else:
                 print "No match found in LoTSS for LBCS calibrator %s"%(lbcsid[x])
-#		print lbcsid[x], lbcsRA[x], lbcsDEC[x]
+
+        result = Table()
+        result['Source_id'] = Source_id
+	result['LBCS_ID'] = LBCS_ID
+        result['LBCS_RA'] = LBCS_RA
+        result['LBCS_DEC'] = LBCS_DEC
+        result['LOTSS_RA'] = LOTSS_RA
+        result['LOTSS_DEC'] = LOTSS_DEC
+        result['Delta_Position'] = Delta_Position
+        result['Total_flux'] = Total_flux
+        result['Peak_flux'] = Peak_flux
+        result['peak_to_total'] = peak_to_total
+        result['Isl_rms'] = Isl_rms
+        result['Resolved'] = Resolved
+
         pass
 
     else:
@@ -207,54 +243,97 @@ def find_close_objs(lo, lb, tolerance=5.):
 
         ## check if it's less than 5 arcsec away
         if delta_position[c_idx] < tolerance:
-            tmp_row=[Source_ID[c_idx][0],lbcsRA,lbcsDEC,lotssRA[c_idx][0],lotssDEC[c_idx][0],np.min(delta_position),lotssflux[c_idx][0],lotsspeakflux[c_idx][0],(lotsspeakflux[c_idx][0]/lotssflux[c_idx][0]), rms[c_idx][0], resolved[c_idx][0] ]
-            result.loc[0] = tmp_row
+            result = Table()
+            result['Source_id'] = [src_id[c_idx][0]]
+            result['LBCS_ID'] = [lbcsid]
+            result['LBCS_RA'] = [lbcsRA]
+            result['LBCS_DEC'] = [lbcsDEC]
+            result['LOTSS_RA'] = [lotssRA[c_idx][0]]
+            result['LOTSS_DEC'] = [lotssDEC[c_idx][0]]
+            result['Delta_Position'] = [np.min(delta_position)]
+            result['Total_flux'] = [lotssflux[c_idx][0]]
+            result['Peak_flux'] = [lotsspeakflux[c_idx][0]]
+            result['peak_to_total'] = [(lotsspeakflux[c_idx][0]/lotssflux[c_idx][0])]
+            result['Isl_rms'] = [rms[c_idx][0]]
+            result['Resolved'] = [resolved[c_idx][0]]
+
         else:
             print "No match found in LoTSS for LBCS calibrator %s"%(lbcsid)
 
     ## convert result to numpy array
     return result
 
-def main( MSname, lotss_radius=5., lbcs_radius=5., bright_limit_Jy=5., lotss_result_file='lotss_catalogue.csv', delay_cals_file='delay_calibrators.csv', subtract_file='subtract_sources.csv', match_tolerance=5., subtract_limit=0.5, doDownload=True ):
+def plugin_main( args, **kwargs ):
+
+    mapfile_in = kwargs['mapfile_in']
+    lotss_radius = kwargs['lotss_radius']
+    lbcs_radius  = kwargs['lbcs_radius']
+    bright_limit_Jy = float(kwargs['bright_limit_Jy'])
+    lotss_result_file = kwargs['lotss_result_file']
+    delay_cals_file = kwargs['delay_cals_file']
+    subtract_file = kwargs['subtract_file']
+    match_tolerance = float(kwargs['match_tolerance'])
+    subtract_limit = float(kwargs['subtract_limit'])
+    image_limit_Jy = float(kwargs['image_limit_Jy'])
+    doDownload = kwargs['doDownload']
+
+    mslist = DataMap.load(mapfile_in)
+    MSname = mslist[0].file
+    
 
     if doDownload:
-        lotss_catalogue = my_lotss_catalogue( MSname[0], Radius=lotss_radius, bright_limit_Jy=bright_limit_Jy )
-        lotss_catalogue = lotss_catalogue.to_pandas()
-        lbcs_catalogue = my_lbcs_catalogue( MSname[0], Radius=lbcs_radius )
-        lbcs_catalogue = lbcs_catalogue.to_pandas()
+        lotss_catalogue = my_lotss_catalogue( MSname, Radius=lotss_radius, bright_limit_Jy=bright_limit_Jy )
+        lbcs_catalogue = my_lbcs_catalogue( MSname, Radius=lbcs_radius )
         result = find_close_objs( lotss_catalogue, lbcs_catalogue, tolerance=match_tolerance )
 
         ## Need to write the following catalogues:
         ## 1 - delay calibrators -- from lbcs_catalogue
-        result.to_csv( delay_cals_file, index=False )
+        result.write( delay_cals_file, format='csv' )
+	print 'wrote'+delay_cals_file
 
         ####### sources to subtract
         ## convert Jy to milliJy
         subtract_index = np.where( result['Total_flux'] > subtract_limit*1e3 )[0]
-        subtract_cals = result[['Source_ID','LOTSS_RA','LOTSS_DEC']].iloc[subtract_index]
-        subtract_cals.columns = ['Source_id','RA','DEC']
-    
+        subtract_cals = result[['Source_id','LOTSS_RA','LOTSS_DEC']][subtract_index]
+	#subtract_cals.rename_column('Source_ID','Source_id')
+	subtract_cals.rename_column('LOTSS_RA','RA')
+	subtract_cals.rename_column('LOTSS_DEC','DEC')
+	
         ## also include bright sources from the lotss catalogue
         ## convert Jy to milliJy
         bright_index = np.where( lotss_catalogue['Total_flux'] >= bright_limit_Jy*1e3 )[0]
-        subtract_bright = lotss_catalogue[['Source_id','RA','DEC']].iloc[bright_index]
+        subtract_bright = lotss_catalogue[['Source_id','RA','DEC']][bright_index]
+	subtract_bright['Source_id'] = subtract_bright['Source_id'].astype(str)
+	## change the data type of the source_id column
+	subtract_bright['tmp'] = subtract_bright['Source_id'].astype(str)
+	subtract_bright.remove_column('Source_id')
+	subtract_bright.rename_column('tmp','Source_id')
+	
+	subtract_sources = vstack( [subtract_cals, subtract_bright])
+	subtract_sources = unique( subtract_sources )
+	subtract_sources.write( subtract_file, format='csv' )
 
-        subtract_sources = pandas.concat( [subtract_cals, subtract_bright] )
-        subtract_sources = subtract_sources.drop_duplicates()
-        subtract_sources.to_csv( subtract_file, index=False )
+        ## sources to image -- first remove things that are already in the delay_cals_file and subtract_file
+	good_index = [ x for x, src_id in enumerate( lotss_catalogue['Source_id'] ) if src_id not in result['Source_id'] and src_id not in subtract_cals['Source_id'] ]
+	
+	tmp_cat = lotss_catalogue[good_index]
 
-        ## sources to image -- let's start with everything that's unresolved
+	## make a flux cut
+	image_index = np.where( tmp_cat['Peak_flux'] >= image_limit_Jy*1e3 )[0]
+	sources_to_image = tmp_cat[image_index]
+
         ## find unresolved
-        nsrcs = float( len( lotss_catalogue['Resolved'] ) )
-        unresolved_index = np.where( lotss_catalogue['Resolved'] == 'U' )[0]
+        nsrcs = float( len( sources_to_image ) )
+	print "There are "+str(nsrcs)+" sources above "+str(image_limit_Jy)+" mJy."
+        unresolved_index = np.where( sources_to_image['Resolved'] == 'U' )[0]
         if nsrcs==0:
             print "Did not find any unresolved objects."
         else:
             perc_unres = len( unresolved_index ) / nsrcs * 100.
             print 'Percentage of sources which are unresolved: '+str( perc_unres )
 
-        sources_to_image = lotss_catalogue.iloc[unresolved_index]
-        sources_to_image.to_csv( lotss_result_file, index=False )
+	sources_to_image.write( lotss_result_file, format='csv' )
+
     else:
 	print "doDownload == False, not downloading catalogues!"
 
@@ -266,15 +345,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument( '--lotss_radius', dest='lotss_radius', type=float, help='Radius to search LoTSS', default=5. )
     parser.add_argument( '--lbcs_radius', dest='lbcs_radius', type=float, help='Radius to search LBCS', default=5. )
-    parser.add_argument( '--lotss_file', dest='lotss_result_file', type=str, help='output file of sources to image', default='lotss_catalogue.csv' )
-    parser.add_argument( '--delay_file', dest='delay_cals_file', type=str, help='output file of delay calibrators', default='delay_calibrators.csv' )
+    parser.add_argument( '--lotss_result_file', dest='lotss_result_file', type=str, help='output file of sources to image', default='lotss_catalogue.csv' )
+    parser.add_argument( '--delay_cals_file', dest='delay_cals_file', type=str, help='output file of delay calibrators', default='delay_calibrators.csv' )
     parser.add_argument( '--subtract_file', dest='subtract_file', type=str, help='output file of sources to subtract', default='subtract_sources.csv' )
     parser.add_argument( '--match_tolerance', dest='match_tolerance', type=float, help='radius for matching LBCS to LoTSS [arcsec]', default=5. )
     parser.add_argument( '--subtract_limit', dest='subtract_limit', type=float, help='Flux limit for sources to subtract [Jy]', default=0.5 )
-    parser.add_argument( '--bright_limit', dest='bright_limit_Jy', type=float, help='Flux limit for bright sources [Jy]', default=5.0 )
-    parser.add_argument( 'MSname', dest='MSname', type=str, nargs='+', help='Measurement set name (to get pointing center)' )
+    parser.add_argument( '--bright_limit_Jy', dest='bright_limit_Jy', type=float, help='Flux limit for bright sources [Jy]', default=5.0 )
+    parser.add_argument( '--image_limit_Jy', dest='image_limit_Jy', type=float, help='Flux limit for which sources to image [Jy]', default = 0.05 )
+    parser.add_argument( 'MSname', type=str, nargs='+', help='Measurement set name (to get pointing center)' )
 
     args = parser.parse_args()
 
-    main( args.MSname, lotss_radius=args.lotss_radius, lbcs_radius=args.lbcs_radius, bright_limit_Jy=args.bright_limit_Jy, lotss_result_file=args.lotss_result_file, delay_cals_file=args.delay_cals_file, subtract_file=args.subtract_file, match_tolerance=args.match_tolerance, subtract_limit=args.subtract_limit )
+    main( args.MSname, lotss_radius=args.lotss_radius, lbcs_radius=args.lbcs_radius, bright_limit_Jy=args.bright_limit_Jy, lotss_result_file=args.lotss_result_file, delay_cals_file=args.delay_cals_file, subtract_file=args.subtract_file, match_tolerance=args.match_tolerance, subtract_limit=args.subtract_limit, image_limit_Jy=args.image_limit_Jy )
 
