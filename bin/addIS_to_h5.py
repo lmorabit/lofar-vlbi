865053265886e843c7fbf637110b71de880717c5
Leah <lmorabit@gmail.com>
2018-11-13 14:18:06 +0000

    updated addIS_to_h5.py so it skips trying to add if solutions already exist

--- bin/addIS_to_h5.py
+++ bin/addIS_to_h5.py
@@ -72,94 +72,97 @@ def main(h5parmfile, MSfiles, solset_in='target', solset_out='targetIS'):
     # Create a new solset for the data
     if not solset_out in data.getSolsetNames():
         new_station_names = makesolset(MSfile, data, solset_out)
-        pass
 
-    # loading solset
-    solset = data.getSolset(solset_in)
-    OutSolset = data.getSolset(solset_out)
-    station_names = solset.getAnt().keys()
+	# loading solset
+        solset = data.getSolset(solset_in)
+        OutSolset = data.getSolset(solset_out)
+        station_names = solset.getAnt().keys()
 
-    # copy existing information for the tables
-    in_soltabNames = solset.getSoltabNames()
-    for soltab_name in in_soltabNames:
-	logging.info('Processing solution table %s'%(soltab_name) )
-        soltab = solset.getSoltab(soltab_name)
-        soltab_axes = soltab.getAxesNames()
-        soltab_type = ''.join([i for i in soltab_name if not i.isdigit()])
-	if soltab_type == 'RMextract':
-	    soltab_type = 'rotationmeasure'
-        if len(soltab_axes) == 2:
-            out_axes = ['ant','time']
-            out_axes_vals = [new_station_names, soltab.time]
-        else:
-            out_axes = ['ant','time','freq','pol']
-            out_axes_vals = [new_station_names, soltab.time, soltab.freq, soltab.pol]
-        ## check number of antennas
-        if len(new_station_names) == soltab.getAxisLen('ant'):
-            ## all stations are present in the solution, just copy
-            for vals, weights, coord, selection in soltab.getValuesIter(returnAxes=soltab_axes, weight=True):
-                vals = reorderAxes( vals, soltab_axes, out_axes )
-                weights = reorderAxes( weights, soltab_axes, out_axes )
-                pass
-            new_soltab = OutSolset.makeSoltab(soltype=soltab_type, soltabName=soltab_name,
-                                        axesNames=out_axes, axesVals=out_axes_vals, vals=vals, weights=weights)
-        else:
-            ## there are new stations in the measurement set, default values have to be added
-            for vals, weights, coord, selection in soltab.getValuesIter(returnAxes=soltab_axes, weight=True):
-                vals = reorderAxes( vals, soltab_axes, out_axes )
-                weights = reorderAxes( weights, soltab_axes, out_axes )
-                pass
-            dimension = np.shape(vals)
-            old_stations = soltab.getAxisValues('ant').tolist()
-
+        # copy existing information for the tables
+        in_soltabNames = solset.getSoltabNames()
+        for soltab_name in in_soltabNames:
+            logging.info('Processing solution table %s'%(soltab_name) )
+            soltab = solset.getSoltab(soltab_name)
+            soltab_axes = soltab.getAxesNames()
+            soltab_type = ''.join([i for i in soltab_name if not i.isdigit()])
+	    if soltab_type == 'RMextract':
+	        soltab_type = 'rotationmeasure'
             if len(soltab_axes) == 2:
-                ## things with a single value per antenna, time
-                new_vals = np.ndarray( shape=(len(new_station_names), dimension[1]) )
-                new_weights = np.ndarray( shape=(len(new_station_names), dimension[1]) )
-                for i, new_station in enumerate(new_station_names):
-                    if new_station in old_stations:
-                        ## antenna exists, copy solutions
-                        ant_index = soltab.getAxisValues('ant', ignoreSelection=True).tolist().index(new_station)
-                        new_vals[i,:] = vals[ant_axis,:]
-                        new_weights[i,:] = weights[ant_axis,:]
-                    else:
-                        ## antenna does not exist, create default solutions
-                        if soltab_type == 'amplitude':
-                            new_vals[i,:] = np.ones(shape=(len(new_station_names), dimension[1]) )
-                            new_weights[i,:] = np.ones(shape=(len(new_station_names), dimension[1]) )
-                        else:
-                            new_vals[i,:] = np.zeros(shape=(len(new_station_names), dimension[1]) )
-                            new_weights[i,:] = np.ones(shape=(len(new_station_names), dimension[1]) )
-                new_soltab = OutSolset.makeSoltab(soltype=soltab_type, soltabName=soltab_name,
-                                            axesNames=out_axes, axesVals=out_axes_vals, vals=new_vals, weights=new_weights)
-
+                out_axes = ['ant','time']
+                out_axes_vals = [new_station_names, soltab.time]
             else:
-                ## things with polarisation and frequency information
-                new_vals = np.ndarray(shape=(len(new_station_names), dimension[1], dimension[2], dimension[3]) )
-                new_weights = np.ndarray(shape=(len(new_station_names), dimension[1], dimension[2], dimension[3]) )
-                for i, new_station in enumerate(new_station_names):
-                    if new_station in old_stations:
-                        ## antenna exists, copy solutions
-                        ant_index = soltab.getAxisValues('ant', ignoreSelection=True).tolist().index(new_station)
-			stuff = vals[ant_index,:,:,:]
-                        new_vals[i,:,:,:] = vals[ant_index,:,:,:]
-                        new_weights[i,:,:,:] = weights[ant_index,:,:,:]
-                    else:
-                        ## antenna does not exist, create default solutions
-                        if soltab_type == 'amplitude':
-                            new_vals[i,:,:,:] = np.ones(shape=(dimension[1], dimension[2], dimension[3]) )
-                            new_weights[i,:,:,:] = np.ones(shape=(dimension[1], dimension[2], dimension[3]))
-                        else:
-                            new_vals[i,:,:,:] = np.zeros(shape=(dimension[1], dimension[2], dimension[3]) )
-                            new_weights[i,:,:,:] = np.ones(shape=(dimension[1], dimension[2], dimension[3]) )
+                out_axes = ['ant','time','freq','pol']
+                out_axes_vals = [new_station_names, soltab.time, soltab.freq, soltab.pol]
+            ## check number of antennas
+            if len(new_station_names) == soltab.getAxisLen('ant'):
+                ## all stations are present in the solution, just copy
+                for vals, weights, coord, selection in soltab.getValuesIter(returnAxes=soltab_axes, weight=True):
+                    vals = reorderAxes( vals, soltab_axes, out_axes )
+                    weights = reorderAxes( weights, soltab_axes, out_axes )
+                    pass
                 new_soltab = OutSolset.makeSoltab(soltype=soltab_type, soltabName=soltab_name,
+                                        axesNames=out_axes, axesVals=out_axes_vals, vals=vals, weights=weights)
+            else:
+                ## there are new stations in the measurement set, default values have to be added
+                for vals, weights, coord, selection in soltab.getValuesIter(returnAxes=soltab_axes, weight=True):
+                    vals = reorderAxes( vals, soltab_axes, out_axes )
+                    weights = reorderAxes( weights, soltab_axes, out_axes )
+                    pass
+                dimension = np.shape(vals)
+                old_stations = soltab.getAxisValues('ant').tolist()
+
+                if len(soltab_axes) == 2:
+                    ## things with a single value per antenna, time
+                    new_vals = np.ndarray( shape=(len(new_station_names), dimension[1]) )
+                    new_weights = np.ndarray( shape=(len(new_station_names), dimension[1]) )
+                    for i, new_station in enumerate(new_station_names):
+                        if new_station in old_stations:
+                            ## antenna exists, copy solutions
+                            ant_index = soltab.getAxisValues('ant', ignoreSelection=True).tolist().index(new_station)
+                            new_vals[i,:] = vals[ant_axis,:]
+                            new_weights[i,:] = weights[ant_axis,:]
+                        else:
+                            ## antenna does not exist, create default solutions
+                            if soltab_type == 'amplitude':
+                                new_vals[i,:] = np.ones(shape=(len(new_station_names), dimension[1]) )
+                                new_weights[i,:] = np.ones(shape=(len(new_station_names), dimension[1]) )
+                            else:
+                                new_vals[i,:] = np.zeros(shape=(len(new_station_names), dimension[1]) )
+                                new_weights[i,:] = np.ones(shape=(len(new_station_names), dimension[1]) )
+                    new_soltab = OutSolset.makeSoltab(soltype=soltab_type, soltabName=soltab_name,
+                                            axesNames=out_axes, axesVals=out_axes_vals, vals=new_vals, weights=new_weights)
+
+                else:
+                    ## things with polarisation and frequency information
+                    new_vals = np.ndarray(shape=(len(new_station_names), dimension[1], dimension[2], dimension[3]) )
+                    new_weights = np.ndarray(shape=(len(new_station_names), dimension[1], dimension[2], dimension[3]) )
+                    for i, new_station in enumerate(new_station_names):
+                        if new_station in old_stations:
+                            ## antenna exists, copy solutions
+                            ant_index = soltab.getAxisValues('ant', ignoreSelection=True).tolist().index(new_station)
+			    stuff = vals[ant_index,:,:,:]
+                            new_vals[i,:,:,:] = vals[ant_index,:,:,:]
+                            new_weights[i,:,:,:] = weights[ant_index,:,:,:]
+                        else:
+                            ## antenna does not exist, create default solutions
+                            if soltab_type == 'amplitude':
+                                new_vals[i,:,:,:] = np.ones(shape=(dimension[1], dimension[2], dimension[3]) )
+                                new_weights[i,:,:,:] = np.ones(shape=(dimension[1], dimension[2], dimension[3]))
+                            else:
+                                new_vals[i,:,:,:] = np.zeros(shape=(dimension[1], dimension[2], dimension[3]) )
+                                new_weights[i,:,:,:] = np.ones(shape=(dimension[1], dimension[2], dimension[3]) )
+                    new_soltab = OutSolset.makeSoltab(soltype=soltab_type, soltabName=soltab_name,
                                             axesNames=out_axes, axesVals=out_axes_vals, vals=new_vals, weights=new_weights)
 
 
-        soltab = 0
-        pass
-    data.close()
-    return(0)
+            soltab = 0
+            pass
+        data.close()
+        return(0)
+    else:
+	data.close()
+	logging.info('Solutions targetIS already exist, not adding to h5parm.')
+	return(0)
 
 
 if __name__ == "__main__":
