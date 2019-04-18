6dc99156c0a18eeddeecc11dca986892ce08291d
Leah <lmorabit@gmail.com>
2019-04-18 10:05:15 +0100

    added plotting functionality back in

--- bin/loop3B_v1.py
+++ bin/loop3B_v1.py
@@ -10,6 +10,7 @@ import astropy.io.fits as pyfits
 from astropy.wcs import WCS
 import time
 import scipy
+from scipy import stats
 import pickle
 import losoto.h5parm as h5parm
 import matplotlib
@@ -17,6 +18,7 @@ matplotlib.use('Agg')
 import astropy
 from matplotlib import pyplot as plt
 import h5py
+import aplpy
 
 CCRIT=1.6 
 TSAMP=4.0    # time per sample. All times are in seconds. TSAMP is passed to NDPPP for writing
@@ -548,6 +550,142 @@ def applycal_split (vis, visA, solset, parmdb, soltab='phase000',\
     fo.close()
     os.system ('NDPPP split.parset')
 
+def fits_axzapit (h, k):
+    try:
+        h.remove(k)
+        return False
+    except:
+        return True
+
+def fits_axzap (infile,outfile):
+    hdulist = pyfits.open(infile)
+    hdu = hdulist[0]
+    while hdu.data.ndim>2:
+        shrinkax = np.argwhere(hdu.data.shape==np.min(hdu.data.shape))[0][0]
+        hdu.data = np.take (hdu.data,0,axis=shrinkax)
+    i=2
+    while True:
+        i+=1
+        isit = fits_axzapit (hdu.header,'CRPIX%d'%i)
+        fits_axzapit (hdu.header,'CTYPE%d'%i)
+        fits_axzapit (hdu.header,'CUNIT%d'%i)
+        fits_axzapit (hdu.header,'CRVAL%d'%i)
+        fits_axzapit (hdu.header,'CDELT%d'%i)
+        fits_axzapit (hdu.header,'CRPIX%d'%i)
+        fits_axzapit (hdu.header,'CROTA%d'%i)
+        hdu.header.update()
+        if isit:
+            break
+    os.system ('rm '+outfile)
+    hdu.writeto (outfile)
+
+def make_plots(vis):
+    import glob
+    imgroot = np.sort(glob.glob(vis+'*MFS-image.fits'))
+    nloop = len(imgroot)
+    h5png = glob.glob(vis+'*h5_*.png')
+    npng = 0
+    for i in h5png:
+        npng = max(npng,int(i.split('_')[-1].split('.')[0])+1)
+    cmd = 'montage -tile %dx%d -geometry 600x600 '%(npng+1,nloop)
+    for i in range(nloop):
+        aplpy_plots( imgroot[i] )
+        thisv = imgroot[i].split('-MFS-image.fits')[0]
+        for j in range(npng):
+            this = '%s_c0.h5_%d.png'%(thisv,j)
+            cmd += (this+' ') if os.path.isfile(this) else 'null: '
+        this = thisv+'-MFS-image.png'
+#        print 'trying to add',this,os.path.isfile(this)
+        cmd += (this+' ') if os.path.isfile(this) else 'null: '
+    cmd += '%s_output.png'%vis
+    print cmd
+    os.system(cmd)
+
+def aplpy_plots( infits, docut=2.0, outpng='', nolabel=False,  crms=3.0, noshift=False, margin=1.7 ):
+
+    sextractor = '/home/jackson/sextractor/usr/bin/sex'
+    with open('default.param','w') as f:
+        f.write('NUMBER\nX_IMAGE\nY_IMAGE\nFLUX_ISO\nFLUX_RADIUS\nFLUX_MAX\nISOAREA_IMAGE\n')
+    f.close()
+    os.system(sextractor+' -d >default.sex')
+    with open('default.conv','w') as f:
+        f.write('CONV NORM\n# 3x3 ``all-ground\'\' convolution mask with FWHM = 2 pixels\n')
+        f.write('1 2 1\n2 4 2\n1 2 1\n')
+    f.close()
+
+    print 'Plotting ',infits
+    fits_axzap (infits,'temp.fits')
+    a = pyfits.getdata('temp.fits')
+    h = pyfits.getheader('temp.fits')
+    nx,ny = h['NAXIS1'],h['NAXIS2']
+    field_radius = h['CDELT2']*ny/2.0
+    nx,ny = a.shape[1],a.shape[0]
+    trms,tmax = np.array([]), np.array([])
+    for i in range(10):
+        x1,x2 = int(0.1*i*nx),int(0.1*(i+1)*nx-1)
+        for j in range(10):
+            y1,y2 = int(0.1*j*ny),int(0.1*(j+1)*ny-1)
+            trms = np.append(trms,np.std(a[y1:y2,x1:x2]))
+            tmax = np.append(tmax,np.std(a[y1:y2,x1:x2]))
+    rms = np.nanmedian(trms)
+    vmin,vmax = np.nanmin(a),np.nanmax(a)
+    pyfits.writeto('temp.fits',a,h,overwrite=True)
+    os.system(sextractor+' temp.fits')
+    s = np.loadtxt('test.cat')
+    s = np.array([s]) if s.ndim==1 else s
+    reqsig = stats.norm.ppf(1-0.5/float(nx*ny)) + (2.0 if docut<-1.0 else docut)
+    s = s[s[:,5]>reqsig*rms]
+    for i in s:
+        print 'Component at (%.1f,%.1f): %f' % (i[1],i[2],i[5])
+    gc = aplpy.FITSFigure('temp.fits')
+    pixra,pixdec = np.mean(s[:,1]),np.mean(s[:,2])
+    dec = h['CRVAL2']+h['CDELT2']*(pixdec-h['CRPIX2'])
+    cosdec = np.cos(np.deg2rad(dec))
+    ra = h['CRVAL1']+h['CDELT1']*(pixra-h['CRPIX1'])/cosdec
+    deccen = h['CRVAL2']+h['CDELT2']*(0.5*ny-h['CRPIX2'])
+    cosdeccen = np.cos(np.deg2rad(deccen))
+    racen = h['CRVAL1']+h['CDELT1']*(0.5*nx-h['CRPIX1'])/cosdeccen
+    pix_range = max(s[:,1].max()-s[:,1].min(),s[:,2].max()-s[:,2].min())
+    deg_range = max(margin*h['CDELT2']*pix_range,0.1*ny*h['CDELT2'])
+    if docut<-1.0:
+        deg_range = -2.0*docut/3600.
+    print 'Range is %.1f arcsec, %.1f pix\n'%(1800.0*deg_range,pix_range)
+    if noshift:
+        gc.recenter(racen,deccen,0.5*deg_range)
+    else:
+        gc.recenter(ra,dec,0.5*deg_range)
+    gc.set_tick_color('black')
+    gc.show_colorscale(cmap=matplotlib.cm.gray_r,vmin=vmin,vmax=vmax)
+    levels, tlevels = [vmax], vmax
+    while tlevels > crms*rms:
+        tlevels /= np.sqrt(2)
+        levels.append(tlevels)
+    gc.show_contour(levels=np.sort(levels))
+    if not nolabel:
+        bstr=''
+        for i in range(len(h)):
+            try:
+                if 'BMAJ' in h[i] and 'AIPS' in h[i]:
+                    bmaj = 3600.*float(h[i].split('BMAJ=')[1].split()[0])
+                    bmin = 3600.*float(h[i].split('BMIN=')[1].split()[0])
+                    bstr = 'beam %.1fx%.1f'%(bmaj,bmin)
+            except:
+                pass
+        if rms>0.1:
+            gc.add_label(0.5,0.05,'Peak %.1f, rms %.1f Jy %s'%\
+                    (vmax,rms,bstr),relative=True,size=14)
+        elif rms>1.e-4:
+            gc.add_label(0.5,0.05,'Peak %.1f, rms %.1f mJy %s'%\
+                    (vmax*1000.,rms*1000.,bstr),relative=True,size=14)
+        else:
+            gc.add_label(0.5,0.05,'Peak %.1f, rms %.1f uJy %s'%\
+                    (vmax*1.e6,rms*1.e6,bstr),relative=True,size=14)
+    if outpng=='':
+        outpng=infits.replace('fits','png')
+    gc.save(outpng)
+    os.system('rm default.conv;rm default.sex;rm default.param;rm temp.fits')
+
+
 def main (vis,strategy='P30,P30,P30,A500,A450,A400',startmod='',ith=5.0,bandwidth='8MHz',goodness=2.):
 
     ## format arguments
@@ -714,6 +852,10 @@ def main (vis,strategy='P30,P30,P30,A500,A450,A400',startmod='',ith=5.0,bandwidt
     ## convert it to a sourcedb
     ss = "makesourcedb in=%s out=%s format='<'"%(skyfile,skyfile.replace('skymodel','sky'))
     os.system(ss)
+
+    ## plot things like solutions
+    make_plots( vis )
+    make_plots( visA )
 
     ## If we got to this point, self-cal has successfully completed    
     montage_plot( '*MFS-image.fits', imscale=0.65, nup='4x2', plot_resid=True)
