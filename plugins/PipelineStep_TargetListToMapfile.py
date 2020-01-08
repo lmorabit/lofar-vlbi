c5770e3fc05a4162da29adf070c657967ca4039b
leah <lmorabit@gmail.com>
2020-01-08 20:50:28 +0000

    updated TargetListToMapfile

--- plugins/PipelineStep_TargetListToMapfile.py
+++ plugins/PipelineStep_TargetListToMapfile.py
@@ -7,74 +7,69 @@ from astropy import units as u
 from astropy.coordinates import SkyCoord
 from astropy.table import Table
 
-
-# Colm Coughlan, March 2016
-# used in locapi generic pipeline implementation
-# Alexander Drabent, January 2017
-# added more functionality 
-# Leah Morabito May 2017 updated to skip header line in manual target file if it exists
-
-
 def plugin_main(args, **kwargs):
     """
-    Takes in list of targets and returns the appropriate one in a mapfile
-    Knows which is the current target by storing target ID in a mapfile
-    Outputs an expanded list of the current target
-
+    Takes in a catalogue with a target and returns an appropriate mapfile
+    
     Parameters
     ----------
-    mapfile_dir : str
+    mapfile_in: str
+        
+    mapfile_dir: str
         Directory for output mapfile
     filename: str
         Name of output mapfile
-    target_list: str
-		List of all targets
-	target_id: str
-		Current 
+    target_file: str
+        file containing target info
 
     Returns
     -------
     result : dict
         Output datamap filename
-
-    """
-    infile_map  = kwargs['mapfile_in']   ## input measurement sets
-    mapfile_dir = kwargs['mapfile_dir']  ## mapfile directory 
-    filename    = kwargs['filename']   ## output measurement sets
-    target_file = kwargs['target_file']  ## file with info on the calibrator
-    data        = DataMap.load(infile_map)	# these are actual MS files
-    datalist    = [data[i].file for i in xrange(len(data))] ## a list of the measurement sets 
-
-    fileid    = os.path.join(mapfile_dir, filename)	           ## the output measurement sets
-    coordfileid   = os.path.join(mapfile_dir, 'coords_'+filename )
-
-    print( fileid )
-    print( coordfileid )
     
-    map_out = DataMap([])
-    map_out_coords = DataMap([])
+    """
+    # parse the inputs
+    infile_map = kwargs['mapfile_in']
+    mapfile_dir = kwargs['mapfile_dir']
+    filename = kwargs['filename']
+    target_file = kwargs['target_file']
+    
+    # the input data
+    data = DataMap.load(infile_map)
+    datalist = [ data[i].file for i in xrange(len(data)) ]
+    
+    # outfile information
+    fileid = os.path.join(mapfile_dir, filename)
+    coordfileid = os.path.join(mapfile_dir, 'coords_' + filename)
+    print fileid
+    print coordfileid
 
-    ## load an astropy table
-    t = Table.read( target_file, format='csv' )
+    # initialise the output data map for the coordinates
+    map_out_coords = DataMap([])
+    # read in the catalogue to get source_id, RA, and DEC
+    t = Table.read(target_file, format='csv')
     RA_val = t['RA_LOTSS'].data[0]
     DEC_val = t['DEC_LOTSS'].data[0]
     Source_id = t['Source_id'].data[0]
+    # make a string of coordinates for the NDPPP command
+    ss = '["' + str(RA_val) + 'deg","' + str(DEC_val) + 'deg"]'
+    # save the coordinate information
+    map_out_coords.data.append(DataProduct(ss, Source_id, False))
+    map_out_coords.save(coordfileid)
+    # save the coords to a variable to return
+    current_coords = map_out_coords[0].host
 
-    ss = '[\"' + str(RA_val) + 'deg\",\"' + str(DEC_val) + 'deg\"]'
-
-    map_out_coords.data.append(DataProduct( ss, Source_id, False ))
-
-    map_out_coords.save(coordfileid)	        # save to file
-    current_coords = map_out_coords[0].host	# save direction
-    current_name = map_out_coords[0].file      # save filename
-
+    # get the name (source_id)
+    current_name = map_out_coords[0].file
+    # initialise an output data map
     map_out = DataMap([])
-    
-    for msID, ms_file in enumerate(datalist): 
-	map_out.data.append(DataProduct( data[msID].host, '/'.join(data[msID].file.split('/')[:-1]) + '/' + current_name + '_' + data[msID].file.split('/')[-1], data[msID].skip)) 
-	pass
-      
-    map_out.save(fileid)			# save all output measurement sets
-    
-    result = {'coordfile':coordfileid,'coords':current_coords,'mapfile':fileid}
+    # loop through input data map and copy information over to output, changing names
+    for msID, ms_file in enumerate(datalist):
+        map_out.data.append(DataProduct(data[msID].host, '/'.join(data[msID].file.split('/')[:-1]) + '/' + current_name + '_' + data[msID].file.split('/')[-1], data[msID].skip))
+    # save the file
+    map_out.save(fileid)
+
+    result = {'coordfile': coordfileid,
+     'coords': current_coords,
+     'mapfile': fileid}
     return result
