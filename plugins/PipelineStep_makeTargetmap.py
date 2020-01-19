lmorabit/lofar-vlbi

commit b7b7eb1ef6ca47a3fd0323cb59ab26b4274b82c8

removed obsolete argument from makeTargetmap

plugins/PipelineStep_makeTargetmap.py
@@ -402,7 +402,6 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--method', help='Method of creating the mapfile.', default='mapfile_from_folder')
     parser.add_argument('-d', '--directory', help='Directory to store the mapfile in.', default='.')
     parser.add_argument('--ddf_solsdir', help='DDF sols directory')
-    parser.add_argument('--prefacet_dir', help='Pre-Factor-Target directory')
     args = parser.parse_args()
     kwarg = {'filename': args.name, 
        'number': args.number, 
@@ -414,7 +413,6 @@ if __name__ == '__main__':
        'method': args.method, 
        'mapfile_dir': args.directory, 
        'ddf_solsdir': args.ddf_solsdir, 
-       'prefacet_dir': args.prefacet_dir}
     if args.number != 0:
         kwarg['method'] = 'dummy'
     if args.suffix:

commit c3d742eff18774666570a1d3635000ca69cb1b3c

Update PipelineStep_makeTargetmap.py
Fix syntax error introduced in commit b7b7eb1ef6ca47a3fd0323cb59ab26b4274b82c8

plugins/PipelineStep_makeTargetmap.py
@@ -412,7 +412,7 @@ if __name__ == '__main__':
        'add_suffix_to_file': args.suffix, 
        'method': args.method, 
        'mapfile_dir': args.directory, 
-       'ddf_solsdir': args.ddf_solsdir, 
+       'ddf_solsdir': args.ddf_solsdir} 
     if args.number != 0:
         kwarg['method'] = 'dummy'
     if args.suffix: