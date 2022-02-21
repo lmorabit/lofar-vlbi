a5a7d4b8f72aa05c6d77b824ca529f25d9b54495
lmorabit <lmorabit@gmail.com>
2022-02-21 15:01:30 +0000

    more troubleshooting

--- bin/prep_selfcal.py
+++ bin/prep_selfcal.py
@@ -14,7 +14,7 @@ import os
 import glob
 import logging
 
-def main(msin,skymod,helperscriptspath='',helperscriptspath-h5merge='',configfile='',destdir=''):
+def main(msin,skymod,helperscriptspath='',helperscriptspath_h5merge='',configfile='',destdir=''):
 
     # copy the config file
     destfile = os.path.join( destdir, configfile.split('/')[-1] )
@@ -24,7 +24,7 @@ def main(msin,skymod,helperscriptspath='',helperscriptspath-h5merge='',configfil
 
     # update it
     os.system( 'sed -i "s~FACETSELFCAL_DIR~{:s}~g" {:s}'.format(helperscriptspath,destfile) )
-    os.system( 'sed -i "s~LOFARHELPERS_DIR~{:s}~g" {:s}'.format(helperscriptspath-h5merge,destfile) )
+    os.system( 'sed -i "s~LOFARHELPERS_DIR~{:s}~g" {:s}'.format(helperscriptspath_h5merge,destfile) )
     os.system( 'sed -i "s~MYMODEL~{:s}~g" {:s}'.format(skymod,destfile) )
     with open( destfile, 'r' ) as f:
         lines = f.readlines()
@@ -36,10 +36,10 @@ if __name__ == "__main__":
     parser.add_argument('msin')
     parser.add_argument('skymod')
     parser.add_argument('--helperscriptspath',default='lofar_facet_selfcal')
-    parser.add_argument('--helperscriptspath-h5merge',default='lofar_helpers')
+    parser.add_argument('--helperscriptspath)h5merge',default='lofar_helpers')
     parser.add_argument('--configfile',default='facetselfcal_config.txt')
     parser.add_argument('--destdir',default='jobdir')
 
     args = parser.parse_args()
 
-    main(args.msin,args.skymod,helperscriptspath=args.helperscriptspath,helperscriptspath-h5merge=args.helperscriptspath-h5merge,configfile=args.configfile,destdir=args.destdir)
+    main(args.msin,args.skymod,helperscriptspath=args.helperscriptspath,helperscriptspath_h5merge=args.helperscriptspath-h5merge,configfile=args.configfile,destdir=args.destdir)
