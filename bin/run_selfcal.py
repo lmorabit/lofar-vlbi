fa622550506eff24dcdd2bbdac066a1d950e4ec1
leah morabito <lmorabit@gmail.com>
2022-02-22 10:31:52 +0000

    use the skynet model

--- bin/run_selfcal.py
+++ bin/run_selfcal.py
@@ -10,10 +10,10 @@ written 21 Feb 2022
 import os
 import glob
 
-def main( msin, skymod, helperscriptspath='', helperscriptspath_h5merge='', configfile='', destdir='' ):
+def main( msin, helperscriptspath='', helperscriptspath_h5merge='', configfile='', destdir='' ):
 
     msin = msin.split('/')[-1]
-    skymod = os.path.join(skymod.split('/')[-2],skymod.split('/')[-1])
+    skymod = os.path.join( msin, 'skymodel' )
 
     with open( 'run_selfcal.log', 'a+') as f:
         f.write('{:s}\n{:s}\n{:s}\n{:s}\n{:s}\n{:s}'.format(msin,skymod,helperscriptspath,helperscriptspath_h5merge,configfile,destdir) )
@@ -36,7 +36,6 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='cleanup script at the end of the pipeline.')
     parser.add_argument('msin')
-    parser.add_argument('skymod')
     parser.add_argument('--helperscriptspath',default='lofar_facet_selfcal')
     parser.add_argument('--helperscriptspath)h5merge',default='lofar_helpers')
     parser.add_argument('--configfile',default='facetselfcal_config.txt')
@@ -44,4 +43,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    main(args.msin,args.skymod,helperscriptspath=args.helperscriptspath,helperscriptspath_h5merge=args.helperscriptspath-h5merge,configfile=args.configfile,destdir=args.destdir)
+    main(args.msin,helperscriptspath=args.helperscriptspath,helperscriptspath_h5merge=args.helperscriptspath-h5merge,configfile=args.configfile,destdir=args.destdir)
