621398101c08daf68db231adbfe4e4d4dfa469ae
James Petley <jwpetley@gmail.com>
2022-11-08 13:24:20 +0000

    Fixes to the solution application

--- bin/run_h5merger.py
+++ bin/run_h5merger.py
@@ -12,14 +12,15 @@ import glob
 
 def main( h5parm, msin, helperscriptspath_h5merge='', ):
 
-    msin = msin.split('/')[-1]
+    #msin = msin.split('/')[-1]
     h5parm_out = h5parm.split('/')[-1].replace('.h5','_toapply.h5')
 
     ## copy the h5_merger.py script
     os.system( 'cp {:s} {:s}'.format( os.path.join( helperscriptspath_h5merge, 'h5_merger.py' ), os.path.join( '.', 'h5_merger.py' ) ) )
-
+    command = '/opt/lofar/pyenv-py2/bin/python h5_merger.py --ms {:s} --h5_tables {:s} --h5_out {:s} --add_cs --h5_time_freq {:s} --circ2lin > convertsols.log'.format( msin, h5parm, h5parm_out, h5parm )
+    print(command)
     ## run on the h5parm using a measurement set for the core station info
-    os.system( '/opt/lofar/pyenv-py2/bin/python h5_merger.py --ms {:s} --h5_tables {:s} --h5_out {:s} --circ2lin --add_cs --h5_time-freq {:s} > convertsols.log'.format( msin, h5parm, h5parm_out, h5parm ) )
+    os.system( '/opt/lofar/pyenv-py2/bin/python h5_merger.py --ms {:s} --h5_tables {:s} --h5_out {:s} --add_cs --h5_time_freq {:s} --circ2lin > convertsols.log'.format( msin, h5parm, h5parm_out, h5parm ) )
 
 if __name__ == "__main__":
 
