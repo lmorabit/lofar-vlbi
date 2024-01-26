c1b84048838c891eae77f0531dff8579267246fa
Leah Morabito <lmorabit@gmail.com>
2024-01-26 12:25:32 +0000

    get php file with wget

--- bin/vlass_search.py
+++ bin/vlass_search.py
@@ -53,6 +53,10 @@ def get_tiles():
     Input: None
     Returns: tile names (numpy str), dec_min (numpy float), dec_max (numpy float), ra_min (numpy float), ra_max (numpy float), observing epoch (numpy str), observation date (numpy str)
     """
+
+    summary_file_location = "VLASS_dyn_summary.php"
+    if not os.path.exists(summary_file_location):
+        os.system('wget https://archive-new.nrao.edu/vlass/VLASS_dyn_summary.php')
     
     names, dec_min, dec_max, ra_min, ra_max, epoch, obsdate = np.loadtxt(summary_file_location, skiprows=3, unpack=True, dtype='str', usecols=(0,1,2,3,4,5,6))
 
@@ -237,6 +241,7 @@ if __name__=="__main__":
     c = SkyCoord(ra, dec, unit='deg')
 
     if not glob.glob(summary_file_location):
-        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), summary_file_location)
+        os.system('wget https://archive-new.nrao.edu/vlass/VLASS_dyn_summary.php')
+        #raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), summary_file_location)
     
     search_vlass(c, crop=crop, crop_scale=crop_scale, consider_QA_rejected=consider_QA_rejected) 
